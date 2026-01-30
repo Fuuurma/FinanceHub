dbde41cb022f1edb0c3d901f657d0d66d23bd46a
Sergi Formatjer <blockintheline@gmail.com>
2026-01-30 12:06:37 +0100

    feat: export InstitutionalHoldingsPanel from research module

--- Frontend/src/components/research/index.ts
+++ Frontend/src/components/research/index.ts
@@ -1,5 +1,6 @@
 export { AnalystRatings } from './AnalystRatings'
 export { InsiderTradingPanel, type InsiderTransaction, type InsiderTransactionType, type InsiderTradingSummary } from './InsiderTradingPanel'
+export { InstitutionalHoldingsPanel, type InstitutionalHoldingData, type HolderType } from './InstitutionalHoldingsPanel'
 export type {
   AnalystRating,
   RatingConsensus,
